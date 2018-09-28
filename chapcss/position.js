34a4542fdb060dae37391bd3b7095bbef57da7cd
fejavu <fejavu@users.noreply.github.com>
2018-09-29 00:13:44 +0800

    MDN

--- chapcss/position.js
+++ chapcss/position.js
@@ -1,28 +1,8 @@
-var tabs = document.querySelectorAll('.info-box li a');
-var panels = document.querySelectorAll('.info-box article');
+var para = document.querySelector('p');
 
-for (var i = 0; i < tabs.length; i++) {
-	var tab = tabs[i];
-	setTabHandler(tab,i);
+para.addEventListener('click',upDateName);
 
-}
-
-function setTabHandler (tab,tabPos) {
-	tab.onclick = function(){
-		for (var i = 0; i < tabs.length; i++) {
-			if(tabs[i].getAttribute('class')){
-				tabs[i].removeAttribute('class');
-			}
-		}
-
-		tab.setAttribute('class','active');
-
-		for (var i = 0; i < panels.length; i++) {
-			if(panels[i].getAttribute('class')){
-				panels[i].removeAttribute('class');
-			}
-		}
-
-		panels[tabPos].setAttribute('class','active-panel');
-	}
+function upDateName () {
+	var name = prompt('Enter a new name');
+	para.innerHTML = 'Player: ' + name;
 }
