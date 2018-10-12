20754f5a0d76d3fc005eb1922541bc3db340903c
fejavu <fejavu@users.noreply.github.com>
2018-10-13 00:53:09 +0800

    MDN

--- javaScript/practice/main.js
+++ javaScript/practice/main.js
@@ -13,14 +13,22 @@ function random(min,max) {
   return num;
 }
 
-function Ball (x,y,velX,velY,color,size) {
+function Shape (x,y,velX,velY,exists) {
 	this.x = x;
 	this.y = y;
 	this.velX = velX;
 	this.velY = velY;
+	this.exists = exists;	
+}
+
+function Ball (x,y,velX,velY,exists,color,size) {
+	Shape.call(this,x,y,velX,velY,exists);
 	this.color = color;
 	this.size = size;	
 }
+
+Ball.prototype = Object.create(Shape.prototype);
+Ball.prototype.constructor = Ball;
 
 Ball.prototype.draw = function() {
 	ctx.beginPath();
@@ -61,13 +69,57 @@ Ball.prototype.collisionDetect = function() {
 			if (distance < this.size + balls[i].size) {
 				balls[i].color = 'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')';
 				this.color = 'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')';
-				balls[i].velX += 1;
-				balls[i].velY += 1;
-				this.velX += 1;
-				this.velY += 1;
+				// balls[i].velX += 1;
+				// balls[i].velY += 1;
+				// this.velX += 1;
+				// this.velY += 1;
 
 			}
 		}
+	}
+}
+
+function EvilCircle (x,y,exists) {
+	// body...
+	Shape.call(this,x,y,exists);
+	this.color = 'white';
+	this.size = 15;
+	this.velX = 20;
+	this.velY = 20;
+}
+
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+
+EvilCircle.prototype.draw = function() {
+	// body...
+	ctx.beginPath();
+	ctx.strokeStyle = this.color;
+	ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+	ctx.stroke();
+}
+
+EvilCircle.prototype.checkBounds = function() {
+
+	// The ball is beyond right bound
+	if ((this.x + this.size)>=width) {
+		this.x -= this.size; 
+	}
+
+	// The ball is beyond left bound
+	if ((this.x - this.size)<=0) {
+		this.x += this.size;
+	}
+
+	// The ball is beyond bottom bound
+	if ((this.y + this.size)>=height) {
+		this.y -= this.size;
+	}
+
+	// The ball is beyond top bound;
+	if ((this.y - this.size)<=0) {
+		// this.velY = -(this.velY);
+		this.y += this.size;
 	}
 }
 
@@ -78,14 +130,15 @@ function loop () {
 	ctx.fillStyle = 'rgba(0,0,0,0.25)';
 	ctx.fillRect(0,0,width,height);
 
-	while(balls.length < 1){
+	while(balls.length < 15){
 		var ball = new Ball(
 			random(0,width),
 			random(0,height),
-			random(-2,2),
-			random(-2,2),
+			random(-8,8),
+			random(-8,8),
+			true,
 			'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')',
-			random(10,20)
+			random(15,30)
 		);
 		balls.push(ball);
 	}
