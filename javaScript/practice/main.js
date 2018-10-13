b90160f0a81f15d07a18479c8daac8bea3a2463b
fejavu <fejavu@users.noreply.github.com>
2018-10-13 11:32:24 +0800

    MDN

--- javaScript/practice/main.js
+++ javaScript/practice/main.js
@@ -1,4 +1,6 @@
 // setup canvas
+var para = document.querySelector('p');
+var count = 0;
 
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
@@ -123,32 +125,82 @@ EvilCircle.prototype.checkBounds = function() {
 	}
 }
 
+EvilCircle.prototype.setControls = function() {
+	// body...
+	var _this = this;
+	window.onkeydown = function(e){
+		if (e.keyCode === 65) {
+			_this.x -= _this.velX;
+		}
+
+		if (e.keyCode === 68) {
+			_this.x += _this.velX;
+		}
+
+		if (e.keyCode === 87) {
+			_this.y -= _this.velY;
+		}
+
+		if (e.keyCode === 83) {
+			_this.y += _this.velY;
+		}
+	}
+}
+
+EvilCircle.prototype.collisionDetect = function() {
+	
+	for (var i = 0; i < balls.length; i++) {
+		if(balls[i].exists){
+			var dx = this.x - balls[i].x;
+			var dy = this.y - balls[i].y;
+			var distance = Math.sqrt(dx*dx+dy*dy);
+
+			if (distance < this.size + balls[i].size) {
+				balls[i].exists = false;
+				count--;
+				para.textContent = 'Ball Count: ' + count;
+			}
+		}
+	}
+}
+
 var balls = [];
+
+var evilCircle = new EvilCircle((width/2),(height/2),true);
+evilCircle.setControls();
 
 function loop () {
 	// body...
 	ctx.fillStyle = 'rgba(0,0,0,0.25)';
 	ctx.fillRect(0,0,width,height);
 
-	while(balls.length < 15){
+	while(balls.length < 5){
 		var ball = new Ball(
 			random(0,width),
 			random(0,height),
-			random(-8,8),
-			random(-8,8),
+			random(5,8),
+			random(5,8),
 			true,
 			'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')',
 			random(15,30)
 		);
+		count++;
 		balls.push(ball);
 	}
 
+	para.textContent = 'Ball Count: '+ count;
+
 	for (var i = 0; i < balls.length; i++) {
-		balls[i].draw();
-		balls[i].update();
-		balls[i].collisionDetect();
+		if (balls[i].exists) {
+			balls[i].draw();
+			balls[i].update();
+			balls[i].collisionDetect();
+		}
 	}
 
+	evilCircle.draw();
+	evilCircle.checkBounds();
+	evilCircle.collisionDetect();
 	requestAnimationFrame(loop);
 }
 
