1298c151803913f365f72cf03158d01b7be7d4e0
fejavu <fejavu@users.noreply.github.com>
2018-10-15 23:09:15 +0800

    MDN

--- javaScript/practice/main.js
+++ javaScript/practice/main.js
@@ -174,7 +174,7 @@ function loop () {
 	ctx.fillStyle = 'rgba(0,0,0,0.25)';
 	ctx.fillRect(0,0,width,height);
 
-	while(balls.length < 5){
+	while(balls.length < 15){
 		var ball = new Ball(
 			random(0,width),
 			random(0,height),
